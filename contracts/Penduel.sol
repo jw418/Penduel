ef043a457d55763691e1eabf9643e65f7b161889
jw418 <ju.wolff@free.fr>
2022-07-26 15:43:13 +0200

    modifying folder structure

--- contracts/Penduel.sol
+++ contracts/Penduel.sol
@@ -3,8 +3,8 @@
 pragma solidity 0.8.15;
 
 // import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "../node_modules/@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "../../node_modules/@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "../../node_modules/@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 /// @title Penduel
 /// @author jw418
