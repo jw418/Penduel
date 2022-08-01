6bb52ecd3852ad471b9693a5e9d2ced53900a94a
jw418 <ju.wolff@free.fr>
2022-08-01 07:43:53 +0200

    update test

--- test/MockPenduel.js
+++ test/MockPenduel.js
@@ -3,6 +3,7 @@ const MockPenduel = artifacts.require(`MockPenduel`);
 // import chai/test-helpers
 const { BN, expectRevert, expectEvent, balance, send, ether,} = require(`@openzeppelin/test-helpers`);
 const { expect } = require(`chai`);
+const constants = require('@openzeppelin/test-helpers/src/constants');
 
 
 
@@ -26,7 +27,7 @@ contract(`MockPenduel`, function (accounts) {
   });
 
   // on vérifie toutes les variables/constantes de notre contrat 
-  context(`###### variable test ######`, () => {
+  
     it(`${testCounter++}: totalCreatedSessions must be equal to 0`, async function () {
       const totalCreatedSessions = await this.MockPenduelInstance.totalCreatedSessions();
       await expect(totalCreatedSessions).to.be.bignumber.equal(
@@ -51,13 +52,13 @@ contract(`MockPenduel`, function (accounts) {
         );
       });
     
-   });
+
 
    // on vérifie toutes les variables/constantes de notre contrat 
-  context(`###### create session test ######`, () => {
+
     
     it(`${testCounter++}: totalCreatedSessions must be equal to 1`, async function () {
-      await this.MockPenduelInstance.createSession({from:  player1, value:betSize});
+      await this.MockPenduelInstance.createSession({from: player1, value:betSize});
       const totalCreatedSessions = await this.MockPenduelInstance.totalCreatedSessions();
       await expect(totalCreatedSessions).to.be.bignumber.equal(
         `1`,
@@ -66,22 +67,23 @@ contract(`MockPenduel`, function (accounts) {
     });
 
     it(`${testCounter++}: should emit an event session created`, async function () {
-        await expectEvent(this.MockPenduelInstance.createSession({from:  player1, value:betSize}), 'SessionCreated', {
-        idSession: '1',
-        playerOne: {player1},
-        betSize: {betSize}
+      //const createSession = await this.MockPenduelInstance.createSession({from:  player1, value:betSize});
+        await expectEvent(await this.MockPenduelInstance.createSession({from:  player1, value:betSize}), 'SessionCreated', {
+        idSession: '2',
+        playerOne: player1,
+        betSize: betSize
         });
     });
 
     it(`${testCounter++}: Expect Revert minimun 1 wei`, async function () {      
-      await expectRevert(this.MockPenduelInstance.createSession({from:  owner, value:'0'}, 'Error, minimum 1 WEI'))
+      await expectRevert(await this.MockPenduelInstance.createSession({from:  player1, value:0}), 'VM Exception while processing transaction: revert Error, minimum 1 WEIError')
     });
 
     it(`${testCounter++}: Expect Revert insufficent vault`, async function () {      
-      await expectRevert(this.MockPenduelInstance.createSession({from:  owner, value: ether("9999999999999999")}, 'Error, insufficent vault balance'))
+      await expectRevert(await this.MockPenduelInstance.createSession({from:  player1, value: ether("9999999999999999")}), 'Error, insufficent vault balance')
     });
     
-   });
+   
    
    
 
